e1e7b00d6e28063d74a50d0ce51f8966dd7b603f
Maria Kolodchenko <mashakolodchenkoo@gmail.com>
2026-05-21 23:35:10 +0300

    implement proxy and JWT, OAuth, API Key

--- lab8.js
+++ lab8.js
@@ -14,3 +14,26 @@ const apiService = {
     return { status: 200, data: `${endpoint}` };
   }
 };
+
+const createAuthProxy = (targetService, authStrategy) => {
+  return new Proxy(targetService, {
+    get(target, prop) {
+      if (prop === 'fetchData') {
+        return async function (endpoint, options = {}) {
+          options.headers = options.headers || {};
+          
+          if (authStrategy.token) {
+            if (authStrategy.type === 'JWT' || authStrategy.type === 'OAuth') {
+              options.headers['Authorization'] = `Bearer ${authStrategy.token}`;
+            } else if (authStrategy.type === 'ApiKey') {
+              options.headers['X-API-Key'] = authStrategy.token;
+            }
+          }
+
+          return target[prop].apply(target, [endpoint, options]);
+        };
+      }
+      return target[prop];
+    }
+  });
+};
