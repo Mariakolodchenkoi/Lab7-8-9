9e547940a0610aee1f7222dedb2e3768a8bd3ee6
Maria Kolodchenko <mashakolodchenkoo@gmail.com>
2026-05-21 16:55:14 +0300

    subscribe and unsubscribe

--- lab7.js
+++ lab7.js
@@ -7,6 +7,21 @@ class EventEmitter {
     if (!this.events[event]) return;
     this.events[event].forEach(listener => listener(data));
   }
+  
+  subscribe(event, listener) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+    return () => this.unsubscribe(event, listener);
+  }
+
+  unsubscribe(event, listenerToRemove) {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter(
+      listener => listener !== listenerToRemove
+    );
+  }
 }
 
 module.exports = EventEmitter;
