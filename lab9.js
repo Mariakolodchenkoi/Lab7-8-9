c15b1a820793263838f0caaa7e5d72b612472f3a
Maria Kolodchenko <mashakolodchenkoo@gmail.com>
2026-05-22 16:30:26 +0300

    performance profiling

--- lab9.js
+++ lab9.js
@@ -8,21 +8,27 @@ function log(config = { level: 'INFO' }) {
       const timestamp = new Date().toISOString();
 
       if (currentLevel >= SYSTEM_LOG_LEVEL) {
-        console.log(`[${timestamp}] [${config.level}] Виклик функції: "${targetFunc.name}"`);
-        console.log(`[${timestamp}] [${config.level}] Аргументи:`, args);
+        console.log(`[${timestamp}] [${config.level}] Calling function: "${targetFunc.name}"`);
+        console.log(`[${timestamp}] [${config.level}] Arguments:`, args);
       }
 
       try {
+        const startTime = performance.now();
+        
         const result = await targetFunc.apply(this, args);
+        
+        const endTime = performance.now(); 
+        const executionTime = (endTime - startTime).toFixed(2);
 
         if (currentLevel >= SYSTEM_LOG_LEVEL) {
-          console.log(`[${new Date().toISOString()}] [${config.level}] Результат:`, result);
+          console.log(`[${new Date().toISOString()}] [${config.level}] Result:`, result);
+          console.log(`[${new Date().toISOString()}] [PROFILE] Function "${targetFunc.name}" executed in ${executionTime}ms`);
         }
         return result;
       } catch (error) {
         console.error(`[${new Date().toISOString()}] [ERROR] Exception in "${targetFunc.name}": ${error.message}`);
-        throw error; 
+        throw error;
+      }
       }
     };
   };
-}
