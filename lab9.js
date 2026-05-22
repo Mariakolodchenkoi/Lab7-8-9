a613339aade48c940619072badd4b22ce4223e87
Maria Kolodchenko <mashakolodchenkoo@gmail.com>
2026-05-22 17:46:36 +0300

    test

--- lab9.js
+++ lab9.js
@@ -45,3 +45,34 @@ function log(config = { level: 'INFO', format: 'text' }) {
     };
   };
 }
+
+function add(a, b) { 
+  return a + b; 
+}
+
+async function fetchUserData(userId) {
+  return new Promise(resolve => setTimeout(() => resolve({ id: userId, role: "KPI Student" }), 300));
+}
+
+function dangerousTask() { 
+  throw new Error("Error: Something went wrong in dangerousTask"); 
+}
+
+const loggedAdd = log({ level: 'INFO', format: 'text' })(add);
+const loggedFetch = log({ level: 'DEBUG', format: 'json' })(fetchUserData);
+const loggedDanger = log({ level: 'ERROR', format: 'text' })(dangerousTask);
+
+(async () => {
+  console.log("Async test (INFO, TEXT)\n");
+  await loggedAdd(25, 17);
+
+  console.log("\nAsync test (DEBUG, JSON)");
+  await loggedFetch("user_id_mariia_kolodchenko");
+
+  console.log("\nConditional error logging test (ERROR)");
+  try {
+    await loggedDanger();
+  } catch (e) {
+    console.log("\nError was logged as expected.");
+  }
+})();
